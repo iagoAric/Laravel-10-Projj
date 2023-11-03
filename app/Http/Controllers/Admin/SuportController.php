134cf0621f2eebd85315033d63d0740a7c8b6153
iagoAric <iagodxt48@hotmail.com>
2023-11-03 17:25:53 -0300

    comit3

--- app/Http/Controllers/Admin/SuportController.php
+++ app/Http/Controllers/Admin/SuportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSuportRequest;
 use App\Models\Support;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,64 @@ class SuportController extends Controller
 {
     public function index(Support $support)
     {
-        $supports = $support->all();
+        $supports = $support->all(); 
 
         return view('admin/supports/index', compact('supports'));
     }
+
+    public function show(string|int $id)
+    {
+        if (!$support = Support::find($id)) {
+            return back();
+        }
+        return view('admin/supports/show', compact('support'));
+    }
+
+    public function create()
+
+    {
+        return view('admin/supports/create');
+    }
+
+    public function store(StoreUpdateSuportRequest $request, Support $support)
+    {
+        $data = $request->all();
+        $data['status'] = 'a'; 
+
+        Support::create($data);
+
+        return redirect()->route('supports.index');
+    }
+
+    public function edit(Support $support, string|int $id)
+    {
+        if(!$support = $support->where('id', $id)->first()){
+
+        }
+        return view('admin/supports.edit', compact('support'));
+    }
+
+    public function update(Request $request, Support $support, string $id)
+    {
+        if (!$support = $support->find($id)){
+            return back();
+        }
+        
+       $support->update($request->only([
+        'subject', 'body'
+       ]));
+
+       return redirect()->route('supports.index');
+    } 
+
+    public function destroy(String|int $id)
+    {
+        if(!$support = Support::find($id)){
+            return back();
+        }
+        $support->delete();
+
+        return redirect()->route('supports.index');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::delete('/supports/{id}', [SuportController::class, 'destroy'])->name('supports.destroy');
+Route::put('/supports/{id}',[SuportController::class, 'update'])->name('supports.update');
+Route::get('/supports/{id}/edit', [SuportController::class, 'edit'])->name('supports.edit');
+Route::get('/supports/create',[SuportController::class, 'create'])->name('supports.create');
+Route::get('/supports/{id}', [SuportController::class, 'show'])->name('supports.show');
+Route::post('/supports', [SuportController::class, 'store'])->name('supports.store');
 Route::get('/supports', [SuportController::class, 'index'])->name('supports.index');
 
 
